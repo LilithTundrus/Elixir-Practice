2872544ee75aa551f33da6c8357b33b8bae960bb
Lilith Tundrus <echofox123@gmail.com>
2018-06-13 19:24:11 -0400

    Working with basic numbers

---
+++ intro/floatingPoints.exs
@@ -0,0 +1,19 @@
+# Floating-point numbers are written using a decimal point.
+# There must be at least one digit before and after the decimal point.
+# An optional trailing exponent may be given
+
+# All of these are valid floating point numbers
+
+# Floats are IEEE 754 double precision, giving them about 16 digits of accuracy
+# and a maximum exponent of around 10308
+
+num1 = 1.0
+num2 = 0.23456
+num3 = 3.14159e1
+num4 = 314_923.014e5
+
+
+IO.puts(num1)
+IO.puts(num2)
+IO.puts(num3)
+IO.puts(num4)
---
+++ intro/integers.exs
@@ -0,0 +1,14 @@
+# Integer literals in Elixir can be written as decimal (1234),
+# hexadecimal (0xcafe), octal (0o765),
+# and binary (0b1010)
+#
+# Decimal numbers may contain underscores—these are often used
+# to separate groups of three digits when writing large numbers,
+# so one million could be written 1_000_000
+#
+# There is no fixed limit on the size of integers
+#
+
+big_number = 1222222231234142141434563636663
+
+IO.puts(big_number)
