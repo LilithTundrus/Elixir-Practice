983d3b75d05878b7bbd4c4eb019e15b2f47a74f8
Lilith Tundrus <echofox123@gmail.com>
2018-06-13 19:55:53 -0400

    atoms example

---
+++ intro/atoms.exs
@@ -0,0 +1,9 @@
+# Atoms are constants that represent something’s name.
+# We write them using a leading colon (:), which can be followed by an
+# atom word or an Elixir operator. An atom word is a sequence of letters,
+# digits, underscores, and at signs (@). It may end with an exclamation
+# point or a question mark. You can also create atoms containing arbitrary
+# characters by enclosing the characters following the colon in double quotes.
+
+:"massive"
+:test
