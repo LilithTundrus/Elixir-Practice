7170bacea1d2cb7e26c2e363967bcaf9e7fa55dc
Lilith Tundrus <echofox123@gmail.com>
2018-06-13 19:58:38 -0400

    Modified this to make it my own

--- intro/scope-basic.exs
+++ intro/scope-basic.exs
@@ -1,12 +1,14 @@
 content = "Test"
 
-lp  =  with {:ok, file}   = File.open("/etc/passwd"),
-            content       = IO.read(file, :all),
-            :ok           = File.close(file),
-            [_, uid, gid] = Regex.run(~r/_lp:.*?:(\d+):(\d+)/, content)
-       do
-            "Group: #{gid}, User: #{uid}"
-       end
+# With is the keyboard that lets us make a temporary var
+lp =
+  with {:ok, file} = File.open("/etc/log"),
+       content = IO.read(file, :all),
+       :ok = File.close(file),
+       # search the file
+       [_, uid, gid] = Regex.run(~r/_lp:.*?:(\d+):(\d+)/, content) do
+    "Group: #{gid}, User: #{uid}"
+  end
 
-IO.puts lp             #=> Group: 26, User: 26
-IO.puts content        #=> Now is the time
+IO.puts(lp)
+IO.puts(content)
