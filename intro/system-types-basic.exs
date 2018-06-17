bfb06e4376a28c773c9da0da7a228166bdd82c28
Lilith Tundrus <echofox123@gmail.com>
2018-06-17 17:03:28 -0400

    Basic system type blurb

---
+++ intro/system-types-basic.exs
@@ -0,0 +1,5 @@
+
+# PIDs + Ports
+# A PID is a reference to a local or remote process. A port is a
+# reference to a resource typically external to the current application
+# that you read from or write to
