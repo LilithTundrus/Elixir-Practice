7064fc160ff949d44a89832cd9de15c183d2ad50
Lilith Tundrus <echofox123@gmail.com>
2018-06-16 23:56:17 -0400

    Finished basic exercise

--- intro/imutability-basic.exs
+++ intro/imutability-basic.exs
@@ -17,3 +17,19 @@ list2 = [4| list1]
 
 # Elixir knows list1 will never change, so it simply constructs a new list
 # with a head of 4 and a tail of list1
+
+
+# Another example of coding with immutability
+
+# Immutable data is surprisingly easy. You just have to remember that any function
+# that transforms data will return a new copy of i
+
+test = "foxxo"
+
+# Not normal like other languages(not test.capitalize)
+capitalizeTest = String.capitalize(test)
+
+# In a functional language, we always transform data. We never modify it in place.
+# The syntax reminds us of this every time we use it
+
+
