bb00ca6e5b97d3efc67516fa56773091709deb56
Lilith Tundrus <echofox123@gmail.com>
2018-06-17 15:37:15 -0400

    Writing up some codeless content for short reference blurbs

---
+++ intro/basic-types.exs
@@ -0,0 +1,25 @@
+# Elixir has a lot of built in types:
+
+# Value types:
+# Arbitrary-sized integers
+# Floating-point numbers
+# Atoms
+# Ranges
+# Regular expressions
+
+
+# System types:
+# PIDs and ports
+# References
+
+
+# Collection types:
+# Tuples
+# Lists
+# Maps
+# Binaries
+
+# Functions are a type as well
+
+# Exixir does not have types like strings or strcutres like most programming languages
+
---
+++ intro/ranges.exs
@@ -0,0 +1,3 @@
+# Ranges are represented as start..end, where start and end are integers
+
+# Think of the [ head | tail ] example
