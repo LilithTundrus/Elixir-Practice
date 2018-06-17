7732f06c771464b39aac9baa54227051e63bbce6
Lilith Tundrus <echofox123@gmail.com>
2018-06-17 17:03:42 -0400

    minor addition

--- intro/system-types-basic.exs
+++ intro/system-types-basic.exs
@@ -3,3 +3,5 @@
 # A PID is a reference to a local or remote process. A port is a
 # reference to a resource typically external to the current application
 # that you read from or write to
+
+# You can get the PID of the current process by calling "self"
