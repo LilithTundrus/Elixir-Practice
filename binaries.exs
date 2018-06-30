7b4fcb0df31c6c32dc865f2178566f10a4fdd1bd
Lilith Tundrus <echofox123@gmail.com>
2018-06-30 17:28:34 -0400

    Finished maps and started binaries

---
+++ binaries.exs
@@ -0,0 +1,7 @@
+# Elixir supports accessing raw data as bits and bytes
+
+# This is both archaic and useful because Elixir treats UTF-8 strings as binary
+
+# Binary literals are enclosed between << and >>
+
+test = <<1, 2, 3>>
--- maps.exs
+++ maps.exs
@@ -10,4 +10,22 @@ test = %{"I" => "Myself", "They" => "Themself"}
 
 responseCodes = %{{:error, :enoent} => :catastrophic, {:error, :busy} => :retry}
 
-IO.puts(responseCodes)
+# Maps can be keys, tuples or atoms, though maps don't always have to be the same type they
+# typically are
+
+# You can also use the atom shortcuts with maps:
+
+# colors = %{ ​red:​ 0xff0000, ​green:​ 0x00ff00, ​blue:​ 0x0000ff }
+
+# Maps only allow for one key per property unlike keyword lists
+
+# You can get the values of a map using its keys, useing square bracket syntax
+
+test = %{"AAA" => "aaa", "BBB" => "bbb"}
+
+test1 = test["AAA"]
+
+# You can also use the typical dot notation if the keys are atoms
+
+
+IO.puts(test["AAA"])
